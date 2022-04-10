402ac27d6378f2eba4ba0075a7d69f117dffc699
jawaluke <jawharlakshmanan6@gmail.com>
2022-04-10 15:38:08 +0530

    added 2 serverjs to out

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/flizzerchat-ui22'));
+app.use(express.static('./dist/flizzerchatUI22'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '/dist/flizzerchat-ui22/'}),
+    res.sendFile('index.html', {root: '/dist/flizzerchatUI22/'}),
 );
 
 // Start the app by listening on the default Heroku port
